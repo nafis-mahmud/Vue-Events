5c47a3c7dc72aa7bac26522637d0abea00b46517
Nafis Mahmud <Nafismahmud181@gmail.com>
2023-08-13 07:17:07 +0600

    4TH commit

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
 
   methods: {
+    setName(event){
+      this.name = event.target.value;
+    },
     add(num){
       this.counter = this.counter +num;
     },
@@ -19,3 +23,5 @@ const app = Vue.createApp({
 });
 
 app.mount('#events');
+
+
